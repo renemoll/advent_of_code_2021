a3e5d494612ac2ab568d9dcbf798ebc585e4917c
René Moll <github@r-moll.nl>
2021-12-13 20:47:43 +0100

    day 7: initial solution

---
+++ day_7/day_7.hs
@@ -0,0 +1,37 @@
+import Data.List
+import Data.List.Split
+
+parse :: String -> [Int]
+parse xs = map (read :: String -> Int) $ splitOn "," xs
+
+-- Generate a list of numbers from x to y (both positive as negative)
+generateRange :: Int -> Int -> [Int]
+generateRange x y = if x <= y
+                    then enumFromTo x y
+                    else reverse $ enumFromTo y x
+
+-- Determine the difference between a fixed number and all elements in a list
+diff :: Int -> [Int] -> [Int]
+diff n xs = map (abs . subtract n) xs
+
+part1 :: [Int] -> Int
+part1 xs = lowest
+  where range = generateRange (minimum xs) (maximum xs)
+        result = map (\a -> sum $ diff a xs) range
+        lowest = minimum result
+
+-- Calculate the costs for each movement
+costs :: [Int] -> [Int]
+costs xs = map (\a -> sum $ generateRange 0 a) xs
+
+part2 :: [Int] -> Int
+part2 xs = lowest
+  where range = generateRange (minimum xs) (maximum xs)
+        result = map (\a -> sum $ costs $ diff a xs) range
+        lowest = minimum result
+
+main :: IO ()
+main = do
+  entries <- parse <$> readFile "input.txt"
+  print $ part1 entries -- 356958
+  print $ part2 entries -- 105461913
