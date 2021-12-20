534deb3885be71c00be3eafb6c5cedcc3964effe
René Moll <rene.moll@demcon.com>
2021-12-20 22:15:36 +0100

    day 12: add solution for the second part

--- src/Day12.hs
+++ src/Day12.hs
@@ -32,26 +32,41 @@ canVisit visited x = case x of
   Start -> False
   End -> False
   Big _ -> True
-  Small x -> not $ (toCave x) `elem` visited
+  Small _ -> not $ x `elem` visited
 
--- Go through the map and determine possible options
-visit :: ([Cave] -> Cave -> Bool) -> Map.Map Cave [Cave] -> Int
+-- Same as canVisit, however now 1 small case may be visited twice
+canVisitAlt :: (Map.Map Cave Int) -> Cave -> Bool
+canVisitAlt visited x = case x of
+  Start -> False
+  End -> False
+  Big _ -> True
+  Small _ -> let
+              isPresent = x `elem` (Map.keys visited)
+              isSmall (Small _) = True
+              isSmall _ = False
+              isAvailable = Map.null $ Map.filterWithKey (\k v -> isSmall k && v > 1) visited
+             in not isPresent || isAvailable
+
+-- Go through the map and determine possible routes
+visit :: (Map.Map Cave Int -> Cave -> Bool) -> Map.Map Cave [Cave] -> Int
 visit p mp = go Map.empty Start End
   where go :: Map.Map Cave Int -> Cave -> Cave -> Int
         go history start end
           | end `elem` neighbours = subresult + 1
           | otherwise = subresult
           where neighbours = mp Map.! start
-                visited = Map.insertWith (+) start 1 history -- does this really need to be a map?
-                options = filter (p (Map.keys visited)) neighbours
+                visited = Map.insertWith (+) start 1 history
+                options = filter (p visited) neighbours
                 subresult = sum $ map (\a -> go visited a end) options
 
-part1 xs = visit canVisit xs
+part1 :: Map.Map Cave [Cave] -> Int
+part1 xs = visit (canVisit . Map.keys) xs
 
-part2 _ = 0
+part2 :: Map.Map Cave [Cave] -> Int
+part2 xs = visit canVisitAlt xs
 
 solve :: String -> (Int, Int)
 solve input = (s1, s2)
   where entries = parse input
         s1 = part1 entries -- 3450
-        s2 = part2 entries --
+        s2 = part2 entries -- 96528
